21fdaada9e03bb3dde1193e833826716d10be4c6
Rashmi-J-K <rashmijk2003@gmail.com>
2023-12-27 07:04:37 +0530

    first

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
-export default function App() {
+const App = () => {
+  const [inputNumber, setInputNumber] = useState('');
+  const [data, setData] = useState([]);
+
+  const handleInputChange = (text) => {
+    setInputNumber(text);
+  };
+
+  const handleSubmit = () => {
+    const number = parseInt(inputNumber, 10);
+
+    if (!isNaN(number)) {
+      const newData = [];
+      for (let i = 1; i <= 10; i++) {
+        newData.push({
+          name: inputNumber,
+          index: i,
+          result: number * i,
+        });
+      }
+      setData(newData);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 20 }}>
+      <TextInput
+        placeholder="Enter a number"
+        keyboardType="numeric"
+        value={inputNumber}
+        onChangeText={handleInputChange}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title>Index</DataTable.Title>
+          <DataTable.Title>Result</DataTable.Title>
+        </DataTable.Header>
+
+        {data.map((item, index) => (
+          <DataTable.Row key={index}>
+            <DataTable.Cell>{item.name}</DataTable.Cell>
+            <DataTable.Cell>{item.index}</DataTable.Cell>
+            <DataTable.Cell>{item.result}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </DataTable>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
